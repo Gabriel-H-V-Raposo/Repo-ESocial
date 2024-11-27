2badcd532fbd8e21c99cf6c1c827837d0d3f94a6
Gabriel Raposo <gabriel.henrique7@hotmail.com>
2024-11-27 14:41:04 -0300

    novas tendencias

--- apps/web/src/app/(app)/orgs/[slug]/(projects)/project-list.tsx
+++ apps/web/src/app/(app)/orgs/[slug]/(projects)/project-list.tsx
@@ -1,5 +1,5 @@
 import { getCurrentOrg } from "@/auth/auth";
-import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -19,6 +19,14 @@ export default async function ProjectList() {
   const currentOrg = getCurrentOrg();
   const { projects } = await getProjects(currentOrg!);
 
+  function getInitials(name: string) {
+    return name
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase())
+      .slice(0, 2)
+      .join("");
+  }
+
   return (
     <div className="grid grid-cols-3 gap-4">
       {projects.map((project) => {
@@ -34,10 +42,13 @@ export default async function ProjectList() {
             </CardHeader>
             <CardFooter className="flex items-center gap-1.5">
               <Avatar className="size-6">
-                {project.avatarUrl ? (
-                  <AvatarImage src={project.avatarUrl} />
-                ) : (
-                  <AvatarImage />
+                {project.owner.avatarUrl && (
+                  <AvatarImage src={project.owner.avatarUrl} />
+                )}
+                {project.owner.name && (
+                  <AvatarFallback>
+                    {getInitials(project.owner.name)}
+                  </AvatarFallback>
                 )}
               </Avatar>
               <span className="truncate text-xs text-muted-foreground">
